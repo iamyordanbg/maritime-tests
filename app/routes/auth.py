870bf60c7f030ce734ec1c3be5ccb89b6d4f69b6
iamyordanbg <iamyordanbg@gmail.com>
2026-06-08 23:57:48 +0200

    5

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -457,3 +457,11 @@ def demo():
             'is_demo': t.is_demo
         })
     return render_template('demo.html', demo_tests=tests_data, recaptcha_site_key=RECAPTCHA_SITE_KEY)
+
+@auth.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@auth.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
