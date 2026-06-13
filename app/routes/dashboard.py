a0d0bb16c0b85aa14ed366eeff98ddea457a2378
iamyordanbg <iamyordanbg@gmail.com>
2026-06-13 07:51:41 +0200

    1

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Blueprint, render_template, request, session, redirect, url_for, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.extensions import db
