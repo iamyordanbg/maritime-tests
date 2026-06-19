862be889b5643f2e347a40fe224bde6457b89dc4
iamyordanbg <iamyordanbg@gmail.com>
2026-06-19 11:14:02 +0200

    2

--- app/static/lighthouse-beam.js
+++ app/static/lighthouse-beam.js
@@ -6,9 +6,25 @@
   var NW=1552,NH=1013,LX=395,LY=285;
   function pos(){
     var cw=hero.clientWidth,ch=hero.clientHeight;
-    var scale,ox=0,oy=0;
-    if(cw/ch>NW/NH){scale=cw/NW;oy=(ch-NH*scale)/2;}
-    else{scale=ch/NH;ox=(cw-NW*scale)/2;}
+    var containerRatio=cw/ch, naturalRatio=NW/NH;
+    var isMobile=cw<=900;
+    // На мобилен object-position се мести към фара (26% center) вместо center center,
+    // затова изчисляваме offset-а по същата логика като CSS object-fit:cover.
+    var focusX=isMobile?0.26:0.5, focusY=0.5;
+    var scale,ox,oy;
+    if(containerRatio>naturalRatio){
+      scale=cw/NW;
+      var visibleH=ch/scale;
+      var cropTop=(NH-visibleH)*focusY;
+      ox=0;
+      oy=-cropTop*scale;
+    } else {
+      scale=ch/NH;
+      var visibleW=cw/scale;
+      var cropLeft=(NW-visibleW)*focusX;
+      ox=-cropLeft*scale;
+      oy=0;
+    }
     pivot.style.left=(ox+LX*scale)+'px';
     pivot.style.top=(oy+LY*scale)+'px';
     pivot.style.transform='scale('+scale+')';
